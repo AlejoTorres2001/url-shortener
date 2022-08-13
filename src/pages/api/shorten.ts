d5634a7a816319a2b2a7e7337edcd33dda3ba528
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-08-13 00:22:12 +0000

    creted createUrl service
    implemented on /shorten api endpoint

--- src/pages/api/shorten.ts
+++ src/pages/api/shorten.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { createUrl } from '../../server/services/createUrl'
 import { shortenUrl } from '../../server/services/shortUrl'
 
 export default async function handler(
@@ -7,20 +8,29 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method !== 'POST') {
-    return res.status(405).json({ statusCode: 405, message: 'Method Not Allowed' })
+    return res
+      .status(405)
+      .json({ statusCode: 405, message: 'Method Not Allowed' })
   }
-  const {url,userEmail} = req.body
+  const { url, userEmail } = req.body
   if (!url) {
     return res.status(400).json({ statusCode: 400, message: 'Missing url' })
   }
   if (!userEmail) {
-    return res.status(400).json({ statusCode: 400, message: 'Missing userEmail' })
+    return res
+      .status(400)
+      .json({ statusCode: 400, message: 'Missing userEmail' })
   }
   try {
-    const shortenedUrl = await shortenUrl(url,userEmail)
-    res.status(200).json({ statusCode: 200, url: shortenedUrl })
-  } catch (error:any) {
+    const shortenedUrl = await shortenUrl(url)
+    const newUrl = await createUrl(
+      shortenedUrl,
+      url as string,
+      userEmail as string
+    )
+    res.status(200).json({ statusCode: 200, shortenUrl: newUrl.shortUrl })
+  } catch (error: any) {
+    console.log(error)
     res.status(500).json({ statusCode: 500, message: error.message })
   }
- 
 }
---
+++ src/server/services/createUrl.ts
@@ -0,0 +1,16 @@
+import { PrismaClient, User } from "@prisma/client"
+import { findUser } from "./findUser"
+
+export const createUrl = async (shortUrl: string,url:string, userEmail: string) =>{
+  const client = new PrismaClient()
+  const match = await findUser(userEmail)
+  if(!match){throw new Error('User not found')}
+  const newUrl = await client.link.create({
+    data: {
+      url: url,
+      shortUrl: shortUrl,
+      UserId: match.id
+    }
+  })
+  return newUrl
+}
