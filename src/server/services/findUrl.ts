5980c3a8532bebb49baa8ccfba5aa16a0f2831e1
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-08-13 15:02:18 +0000

    added findUrl service

---
+++ src/server/services/findUrl.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from "@prisma/client"
+
+export const findUrl = async (url: string) => {
+  const client = new PrismaClient()
+  const match = await client.link.findUnique({ where: { url: url } })
+  return match
+}
