3d4f01c7d590f0d1db4f496edb1b104f59cc9548
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-08-13 14:51:51 +0000

    created signup endpoint

---
+++ src/pages/api/signup.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { CompareHashes } from '../../server/services/CompareHashes'
+import { findUser } from '../../server/services/findUser'
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'POST') {
+    res.status(405).json({ message: 'Method not allowed' })
+    return
+  }
+  const { email, password }: { password: string; email: string } = req.body
+  if (!email) {
+    res.status(400).json({ message: 'Email is required' })
+    return
+  }
+  if (!password) {
+    res.status(400).json({ message: 'Password is required' })
+    return
+  }
+  const userRegister = await findUser(email)
+  if (userRegister) {
+    try {
+      const match = await CompareHashes(password, userRegister.password)
+      if (match) {
+        res.status(200).json({ message: 'user logged in' })
+        return
+      }
+    } catch (error: any) {
+      res.status(500).json({ message: error.message })
+      return
+    }
+  }
+  res.status(400).json({ message: 'invalid username or passoword' })
+}
