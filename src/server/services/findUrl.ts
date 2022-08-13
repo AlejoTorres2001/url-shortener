95528666513bd94a39f48e4cff4bbfff13866062
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-08-13 15:51:38 +0000

    disconnected client at last

--- src/server/services/findUrl.ts
+++ src/server/services/findUrl.ts
@@ -3,5 +3,6 @@ import { PrismaClient } from "@prisma/client"
 export const findUrl = async (url: string) => {
   const client = new PrismaClient()
   const match = await client.link.findUnique({ where: { url: url } })
+  client.$disconnect()
   return match
 }
