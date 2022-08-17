20f843dc0c1c19d29f2993dcb2bf9bd57c192dc5
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-08-17 15:26:05 +0000

    using hook on component

--- src/components/RegisterUserFieldSet.tsx
+++ src/components/RegisterUserFieldSet.tsx
@@ -1,15 +1,41 @@
 import React from 'react'
+import useRegisterField, { registerFieldProps } from '../hooks/useRegisterField'
 
-const RegisterUserFieldSet = ({setActiveFieldset}) => {
+const RegisterUserFieldSet = ({ setActiveFieldset }: registerFieldProps) => {
+  const {
+    EmailRef,
+    passwordRef,
+    confirmPasswordRef,
+    HandleUserRegister,
+    isError
+  } = useRegisterField({ setActiveFieldset })
   return (
     <fieldset>
-    <h2 className="fs-title">Create your account</h2>
-    <h3 className="fs-subtitle">This will only take you seconds</h3>
-    <input type="text" name="email" placeholder="Email" />
-    <input type="password" name="pass" placeholder="Password" />
-    <input type="password" name="cpass" placeholder="Confirm Password" />
-    <input type="button" name="next" className="next action-button" value="Next" onClick={()=> setActiveFieldset(2)} />
-  </fieldset>
+      <h2 className="fs-title">Create your account</h2>
+      <h3 className="fs-subtitle">This will only take you seconds</h3>
+      <input ref={EmailRef} type="text" name="email" placeholder="Email" />
+      <input
+        ref={passwordRef}
+        type="password"
+        name="pass"
+        placeholder="Password"
+      />
+      <input
+        ref={confirmPasswordRef}
+        type="password"
+        name="cpass"
+        placeholder="Confirm Password"
+      />
+      <input
+        type="button"
+        name="next"
+        className="next action-button"
+        value="Next"
+        onClick={HandleUserRegister}
+      />
+      {isError && <div>UPS!theres been an error</div>}
+      <a onClick={() => setActiveFieldset(2)}>already have an account?</a>
+    </fieldset>
   )
 }
 
