77e104acc82eecd986861772b97ff73fbcfa674d
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-08-21 00:24:33 +0000

    Moved authcontext to src folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import AuthContextProvider from '../../context/authContext'
+import AuthContextProvider from '../context/authContext'
 function MyApp({ Component, pageProps }: AppProps) {
   const client = new QueryClient()
   return (
