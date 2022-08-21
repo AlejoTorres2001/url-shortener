802cb2d8565c979449babeaf3771ef7c0e1e982c
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-08-21 00:38:01 +0000

    added deployment url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosInstance } from 'axios';
-const DEPLOY_URL =''
+const DEPLOY_URL ='https://url-shortner-two-psi.vercel.app/api'
 export const api:AxiosInstance = axios.create({
   baseURL: process.env.NODE_ENV === 'development'? 'http://localhost:3000/api' : DEPLOY_URL
 })
